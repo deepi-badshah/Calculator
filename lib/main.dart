a2fb4b79f2f656f48194f414121a33c558cdf571
Deepanshu <b420021@iiit-bh.ac.in>
2022-12-17 06:19:50 +0530

    Finalised

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:basic_calculator/my_button.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,9 +14,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var userInput = 'Deepanshu';
+  var userInput = '';
 
-  var answer = 'Rajput';
+  double answer = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -38,24 +39,35 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(children: [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
             child: Expanded(
               flex: 2,
-              child: Center(
-                child: Column(children: [
-                  Text(
-                    userInput.toString(),
-                    style: const TextStyle(color: Colors.white, fontSize: 25),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    answer.toString(),
-                    style: const TextStyle(color: Colors.white, fontSize: 35),
-                  ),
-                ]),
-              ),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text(
+                        userInput.toString(),
+                        textAlign: TextAlign.end,
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text(
+                        answer.toString(),
+                        textAlign: TextAlign.end,
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 35),
+                      ),
+                    ),
+                  ]),
             ),
           ),
           Expanded(
@@ -67,42 +79,182 @@ class _MyAppState extends State<MyApp> {
                 children: [
                   Row(
                     children: [
-                      MyButton(content: 'AC', hcolor: Colors.orange),
-                      MyButton(content: 'DEL', hcolor: Colors.orange),
-                      MyButton(content: '%', hcolor: Colors.orange),
-                      MyButton(content: '/', hcolor: Colors.orange),
+                      MyButton(
+                        content: 'AC',
+                        hcolor: Colors.orange,
+                        press: () {
+                          userInput = '';
+                          answer = 0.0;
+                          setState(() {});
+                        },
+                      ),
+                      MyButton(
+                        content: 'DEL',
+                        hcolor: Colors.orange,
+                        press: () {
+                          userInput =
+                              userInput.substring(0, userInput.length - 1);
+                          setState(() {});
+                        },
+                      ),
+                      MyButton(
+                        content: '%',
+                        hcolor: Colors.orange,
+                        press: () {
+                          userInput += '%';
+                          setState(() {});
+                        },
+                      ),
+                      MyButton(
+                        content: '/',
+                        hcolor: Colors.orange,
+                        press: () {
+                          userInput += '/';
+                          setState(() {});
+                        },
+                      ),
                     ],
                   ),
                   Row(
                     children: [
-                      MyButton(content: '1', hcolor: Colors.grey),
-                      MyButton(content: '2', hcolor: Colors.grey),
-                      MyButton(content: '3', hcolor: Colors.grey),
-                      MyButton(content: 'x', hcolor: Colors.orange),
+                      MyButton(
+                        content: '1',
+                        hcolor: Colors.grey,
+                        press: () {
+                          userInput += '1';
+                          setState(() {});
+                        },
+                      ),
+                      MyButton(
+                        content: '2',
+                        hcolor: Colors.grey,
+                        press: () {
+                          userInput += '2';
+                          setState(() {});
+                        },
+                      ),
+                      MyButton(
+                        content: '3',
+                        hcolor: Colors.grey,
+                        press: () {
+                          userInput += '3';
+                          setState(() {});
+                        },
+                      ),
+                      MyButton(
+                        content: 'x',
+                        hcolor: Colors.orange,
+                        press: () {
+                          userInput += 'x';
+                          setState(() {});
+                        },
+                      ),
                     ],
                   ),
                   Row(
                     children: [
-                      MyButton(content: '4', hcolor: Colors.grey),
-                      MyButton(content: '5', hcolor: Colors.grey),
-                      MyButton(content: '6', hcolor: Colors.grey),
-                      MyButton(content: '-', hcolor: Colors.orange),
+                      MyButton(
+                        content: '4',
+                        hcolor: Colors.grey,
+                        press: () {
+                          userInput += '4';
+                          setState(() {});
+                        },
+                      ),
+                      MyButton(
+                        content: '5',
+                        hcolor: Colors.grey,
+                        press: () {
+                          userInput += '5';
+                          setState(() {});
+                        },
+                      ),
+                      MyButton(
+                        content: '6',
+                        hcolor: Colors.grey,
+                        press: () {
+                          userInput += '6';
+                          setState(() {});
+                        },
+                      ),
+                      MyButton(
+                        content: '-',
+                        hcolor: Colors.orange,
+                        press: () {
+                          userInput += '-';
+                          setState(() {});
+                        },
+                      ),
                     ],
                   ),
                   Row(
                     children: [
-                      MyButton(content: '7', hcolor: Colors.grey),
-                      MyButton(content: '8', hcolor: Colors.grey),
-                      MyButton(content: '9', hcolor: Colors.grey),
-                      MyButton(content: '+', hcolor: Colors.orange),
+                      MyButton(
+                        content: '7',
+                        hcolor: Colors.grey,
+                        press: () {
+                          userInput += '7';
+                          setState(() {});
+                        },
+                      ),
+                      MyButton(
+                        content: '8',
+                        hcolor: Colors.grey,
+                        press: () {
+                          userInput += '8';
+                          setState(() {});
+                        },
+                      ),
+                      MyButton(
+                        content: '9',
+                        hcolor: Colors.grey,
+                        press: () {
+                          userInput += '1';
+                          setState(() {});
+                        },
+                      ),
+                      MyButton(
+                        content: '+',
+                        hcolor: Colors.orange,
+                        press: () {
+                          userInput += '+';
+                          setState(() {});
+                        },
+                      ),
                     ],
                   ),
                   Row(
                     children: [
-                      MyButton(content: '.', hcolor: Colors.orange),
-                      MyButton(content: '0', hcolor: Colors.grey),
-                      MyButton(content: '00', hcolor: Colors.orange),
-                      MyButton(content: '=', hcolor: Colors.orange),
+                      MyButton(
+                        content: '.',
+                        hcolor: Colors.orange,
+                        press: () {
+                          userInput += '.';
+                          setState(() {});
+                        },
+                      ),
+                      MyButton(
+                          content: '0',
+                          hcolor: Colors.grey,
+                          press: () {
+                            userInput += '0';
+                            setState(() {});
+                          }),
+                      MyButton(
+                          content: '00',
+                          hcolor: Colors.orange,
+                          press: () {
+                            userInput += '00';
+                            setState(() {});
+                          }),
+                      MyButton(
+                        content: '=',
+                        hcolor: Colors.orange,
+                        press: () {
+                          answer = equalPress(userInput);
+                          setState(() {});
+                        },
+                      ),
                     ],
                   ),
                 ],
@@ -114,4 +266,16 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
+}
+
+double equalPress(String userInput) {
+  String finalIn = userInput;
+  finalIn = userInput.replaceAll('x', '*');
+
+  Parser p = Parser();
+  Expression exp = p.parse(finalIn);
+  ContextModel cm = ContextModel();
+
+  double eval = exp.evaluate(EvaluationType.REAL, cm);
+  return eval;
 }
--- lib/my_button.dart
+++ lib/my_button.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:basic_calculator/main.dart';
 
 class MyButton extends StatelessWidget {
   var content = '';
   Color hcolor = Colors.black;
+  final VoidCallback press;
   MyButton({
     super.key,
+    required this.press,
     required this.content,
     required this.hcolor,
   });
@@ -15,14 +16,11 @@ class MyButton extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: OutlinedButton(
-          onPressed: (() {
-            //userInput += '5';
-          }),
+          onPressed: () => press(),
           style: OutlinedButton.styleFrom(
               fixedSize: const Size(10, 50),
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10)),
-              //foregroundColor: Colors.white,
               side: BorderSide(color: hcolor)),
           child: Text(
             content,
